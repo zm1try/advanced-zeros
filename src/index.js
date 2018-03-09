9d5610628b6518791e8316fa57dd48025cc363f6
zm1try <zmitryarosh@gmail.com>
2018-03-09 18:36:35 +0300

    100/100

--- src/index.js
+++ src/index.js
@@ -1,50 +1,37 @@
 module.exports = function getZerosCount(number, base) {
 
-var count = 0, k, d = 0;
-var arr = [];
+  let result = [],
+      pow,
+      zeros,
+      _base = base,
+      _number;
 
-function isPrime(num) {
-  if(num < 2) return false;
-  for (let i = 2; i < num; i++) {
-      if(num%i==0)
-          return false;
-  }
-  return true;
-}
+  for (let i = 2; i <= base; i++) {
 
+    if (_base % i === 0) {
+      
+      pow = 0;
 
-for (let i = 2; i<=base; i++) {
-  if (isPrime(i) && base%i === 0) {
-    k = i;
-    while(k<=number) {
-      count += Math.floor(number/k);
-      k = k*i;
+      while (_base % i === 0) {
+        pow++;
+        _base = ~~(_base/i);
+      }
 
+      _number = number;
+      zeros = 0;
+
+      while (_number/i > 0) {
+        zeros += ~~(_number/i);
+        _number = ~~(_number/i);
+      }
+
+      result.push(~~(zeros/pow));
     }
-    arr.push(count);
-    count = 0;
-    console.log(i);
   }
-}
-var temp = base;
-var arrtemp = [];
-for (let i = 2; i<=temp; i++) {
-  if (isPrime(i) && temp%i === 0) {
-    arrtemp.push(i);
-    temp = temp/i;
-    i=1;
-  }
-}
-console.log(arrtemp);
-var minus = 1;
-for(let i = arrtemp.length-1; i>0;i--) {
-  if (arrtemp[i] === arrtemp[i-1]) minus++;
-  else i=0;
-}
-console.log(minus);
+  
+  result.sort(function(a, b) {
+    return a - b;
+  });
 
-return Math.floor(arr[arr.length-1]/minus);
-
-
-
+  return result[0];
 }
